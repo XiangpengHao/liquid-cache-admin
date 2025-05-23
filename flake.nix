9a363a7e095a4c55780c99bd1fd2937ace59e1c0
Xiangpeng Hao <me@xiangpeng.systems>
2025-05-23 09:56:53 -0500

    update flake

---
+++ flake.nix
@@ -0,0 +1,41 @@
+{
+  description = "Liquid Cache Flake Configuration";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    { nixpkgs
+    , rust-overlay
+    , flake-utils
+    , ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+      in
+      {
+        devShells.default = with pkgs;
+          mkShell {
+            buildInputs = [
+              openssl
+              pkg-config
+              eza
+              fd
+			        trunk
+              llvmPackages.bintools
+              (rust-bin.nightly.latest.default.override {
+                targets = [ "wasm32-unknown-unknown" ];
+              })
+            ];
+          };
+      }
+    );
+}
